1ac661213ecb4f836b71aaed64bbecad0216af85
timpt0261 <rchavez0215@gmail.com>
2023-11-27 10:16:23 -0800

    Adding Itch.io configuration

--- git_sha_project_setting/plugin.gd
+++ git_sha_project_setting/plugin.gd
@@ -7,26 +7,26 @@ const Utils := preload("utils.gd")
 var export_plugin: ExportPlugin
 
 func _disable_plugin() -> void:
-    ProjectSettings.set_setting(Utils.GIT_SHA_PATH, null)
-    ProjectSettings.save()
+	ProjectSettings.set_setting(Utils.GIT_SHA_PATH, null)
+	ProjectSettings.save()
 
 func _enter_tree() -> void:
-    Utils.init_project_setting(Utils.GIT_SHA_PATH, "", TYPE_STRING, PROPERTY_HINT_NONE)
-    ProjectSettings.set_as_internal(Utils.GIT_SHA_PATH, true)
-    ProjectSettings.save()
+	Utils.init_project_setting(Utils.GIT_SHA_PATH, "", TYPE_STRING, PROPERTY_HINT_NONE)
+	ProjectSettings.set_as_internal(Utils.GIT_SHA_PATH, true)
+	ProjectSettings.save()
 
-    self.export_plugin = ExportPlugin.new()
-    self.add_export_plugin(self.export_plugin)
+	self.export_plugin = ExportPlugin.new()
+	self.add_export_plugin(self.export_plugin)
 
-    Utils.update_git_sha()
-    ProjectSettings.save()
+	Utils.update_git_sha()
+	ProjectSettings.save()
 
 func _exit_tree() -> void:
-    self.remove_export_plugin(self.export_plugin)
-    ProjectSettings.set_setting(Utils.GIT_SHA_PATH, null)
-    ProjectSettings.save()
+	self.remove_export_plugin(self.export_plugin)
+	ProjectSettings.set_setting(Utils.GIT_SHA_PATH, null)
+	ProjectSettings.save()
 
 func _build() -> bool:
-    Utils.update_git_sha()
-    ProjectSettings.save()
-    return true
+	Utils.update_git_sha()
+	ProjectSettings.save()
+	return true
