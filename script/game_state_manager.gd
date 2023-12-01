5ad194e9ab3e35fb3feb5c47f051d39bbd6b1d68
timpt0261 <rchavez0215@gmail.com>
2023-12-01 13:29:58 -0800

    gsa include turn count and score count

--- script/game_state_manager.gd
+++ script/game_state_manager.gd
@@ -1,12 +1,11 @@
 extends Node2D
 class_name game_state_manager
 
-@onready var terrain_map = $TerrainMap
-@onready var plant_manager = $TerrainMap/Plants
-@onready var player = $TerrainMap/Player
-@onready var tunr_count = $"../Labels/TurnCount"
-@onready var score_count = $"../Labels/ScoreCount"
-@onready var MASTER_GRID_SIZE = terrain_map.MASTER_GRID_SIZE
+@onready var terrain_map = $TerrainMap;
+@onready var plant_manager = $TerrainMap/Plants;
+@onready var player = $TerrainMap/Player;
+@onready var turn_count = $"../Labels/TurnCount";
+@onready var MASTER_GRID_SIZE = terrain_map.MASTER_GRID_SIZE;
 
 var cell_state_array: Array;
 var cell_state_byte_array: PackedByteArray
@@ -14,15 +13,21 @@ var cell_state_byte_array: PackedByteArray
 func _ready():
 	cell_state_array = [];
 	cell_state_array.resize(MASTER_GRID_SIZE*MASTER_GRID_SIZE)
+	game_state_to_array()
 	
 func game_state_to_array():
 	#player_pos first 12 bytes
 	var player_pos = player.position;
+	var turn_number = turn_count.turn_number;
+	var score_number = plant_manager.get_plant_harvested_amount();
 	var terrain_dict = terrain_map.terrain_dict
 	var plant_dict = plant_manager.plantDict
 	cell_state_byte_array = PackedByteArray()
 	
 	cell_state_byte_array.append_array(var_to_bytes(player_pos))
+	cell_state_byte_array.append_array(var_to_bytes(turn_count.turn_number))
+	cell_state_byte_array.append_array(var_to_bytes(score_number))
+	
 	
 	#32 bytes per cell
 	for y in MASTER_GRID_SIZE:
@@ -75,30 +80,26 @@ func do_load():
 func load_cell_state(byte_array: PackedByteArray):
 	# Clear existing state (if any) before loading
 	cell_state_array.clear()
+#	update_game_state()
 
-	# still working out
-
-	# Update the game state using the loaded data
-	update_game_state()
-
-func update_game_state():
-	# Update the terrain, plants, player, and other game state elements
-	# based on the loaded cell state array
-	var MASTER_GRID_SIZE = terrain_map.MASTER_GRID_SIZE
-	for i in range(MASTER_GRID_SIZE * MASTER_GRID_SIZE):
-		var pos = Vector2(i % MASTER_GRID_SIZE, i / MASTER_GRID_SIZE)
-		var g = terrain_map.terrain_dict[pos]
-		var cell_data = cell_state_array[i]
-
-		# Update terrain and plant information
-		g.set_wetness(cell_data.hydration)
-		g.set_sunlight(cell_data.sun)
-
-		if cell_data.plant_type_id >= 0:
-			plant_manager.plant_plant(pos, cell_data.plant_type_id, cell_data.plant_growth)
-
-	# Additional game state updates based on the loaded data
-	var additional_data = cell_state_array[-1]  # Get the last element in the array
-	tunr_count.text = str(additional_data.turn_count)
-	score_count.text = str(additional_data.score_count)
-
+#func update_game_state():
+#	# Update the terrain, plants, player, and other game state elements
+#	# based on the loaded cell state array
+#	var MASTER_GRID_SIZE = terrain_map.MASTER_GRID_SIZE
+#	for i in range(MASTER_GRID_SIZE * MASTER_GRID_SIZE):
+#		var pos = Vector2(i % MASTER_GRID_SIZE, i / MASTER_GRID_SIZE)
+#		var g = terrain_map.terrain_dict[pos]
+#		var cell_data = cell_state_array[i]
+#
+#		# Update terrain and plant information
+#		g.set_wetness(cell_data.hydration)
+#		g.set_sunlight(cell_data.sun)
+#
+#		if cell_data.plant_type_id >= 0:
+#			plant_manager.plant_plant(pos, cell_data.plant_type_id, cell_data.plant_growth)
+#
+#	# Additional game state updates based on the loaded data
+#	var additional_data = cell_state_array[-1]  # Get the last element in the array
+#	tunr_count.text = str(additional_data.turn_count)
+#	score_count.text = str(additional_data.score_count)
+#
--- script/score_count.gd
+++ script/score_count.gd
@@ -2,7 +2,6 @@ extends Label
 
 @onready var plant_manager = $"../../game_state_manager/TerrainMap/Plants";
 
-
 func _ready():
 	# Connect to the signal emitted when a flower is harvested
 	plant_manager.connect("harvested_plant", Callable(self, "_on_plant_harvested"));
